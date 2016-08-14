1d0d439b09a16e8829fabc700d92fd533223ff7a
jc320816 <tristin.hillebrand@my.jcu.edu.au>
2016-08-15 09:40:56 +1000

    add name printing program

--- sandbox 1.py
+++ sandbox 1.py
@@ -1,3 +1,10 @@
-""""
+"""
 i dont know what im doing
-""""
+"""
+
+name = input("please enter your name: ")
+while len(name) == 0:
+    name = input("please enter your name: ")
+
+for i in range(0, len(name), 2):
+    print(name[i], end=" ")
